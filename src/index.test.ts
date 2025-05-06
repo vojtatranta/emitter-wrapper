5ae85fc7a24fdf5bb53a976a8e1b21e63b2074bc
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-05-06 20:10:12 +0200

    initial: working prototype

---
+++ src/index.test.ts
@@ -0,0 +1,142 @@
+/* eslint-disable @typescript-eslint/await-thenable */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { EmitterWrapper } from "./index";
+import events from "events";
+
+describe("EmitterWrapper type behavior", () => {
+  class ConcreteEmitter extends events.EventEmitter {
+    getState(): 0 | "state-one" {
+      return Math.random() > 0.5 ? "state-one" : 0;
+    }
+  }
+
+  it("getState returns correct union type", () => {
+    const wrapper = EmitterWrapper.wrap(new ConcreteEmitter());
+    const state = wrapper.getState();
+    // Type assertions (runtime):
+    expect([0, "state-one"]).toContain(state);
+  });
+
+  it("inState callback receives correct state value", () => {
+    const wrapper = EmitterWrapper.wrap(new ConcreteEmitter());
+    let callbackValue: 0 | "state-one" | undefined;
+
+    wrapper.inState("state-one", (s) => {
+      callbackValue = s;
+      expect(s).toBe("state-one");
+    });
+    wrapper.inState(0, (s) => {
+      callbackValue = s;
+      expect(s).toBe(0);
+    });
+  });
+
+  it("promised returns a Promise", async () => {
+    const wrapper = EmitterWrapper.wrap(new ConcreteEmitter());
+    const promise = wrapper.promised("state-one");
+    expect(promise).toBeInstanceOf(Promise);
+    // We cannot guarantee resolution here due to randomness, but we can check type
+    await expect(promise.catch(() => {})).resolves;
+  });
+
+  it("calls callback immediately if already in desired state", () => {
+    class MyEmitter extends events.EventEmitter {
+      private _state = "ready";
+      getState() {
+        return this._state;
+      }
+      setState(newState: string) {
+        this._state = newState;
+        this.emit("stateChange");
+      }
+    }
+    const emitter = new MyEmitter();
+    const wrapper = EmitterWrapper.wrap(emitter);
+    let called = false;
+    wrapper.inState("ready", (s) => {
+      called = true;
+      expect(s).toBe("ready");
+    });
+    expect(called).toBe(true);
+  });
+
+  it("calls callback when state changes to desired value", () => {
+    class MyEmitter extends events.EventEmitter {
+      private _state = "idle";
+      getState() {
+        return this._state;
+      }
+      setState(newState: string) {
+        this._state = newState;
+        this.emit("stateChange");
+      }
+    }
+    const emitter = new MyEmitter();
+    const wrapper = EmitterWrapper.wrap(emitter);
+    let called = false;
+    wrapper.inState("ready", (s) => {
+      called = true;
+      expect(s).toBe("ready");
+    });
+    expect(called).toBe(false);
+    emitter.setState("ready");
+    expect(called).toBe(true);
+  });
+
+  it("removes event listener after callback is called", () => {
+    class MyEmitter extends events.EventEmitter {
+      private _state = "idle";
+      getState() {
+        return this._state;
+      }
+      setState(newState: string) {
+        this._state = newState;
+        this.emit("stateChange");
+      }
+    }
+    const emitter = new MyEmitter();
+    const wrapper = EmitterWrapper.wrap(emitter);
+    let callCount = 0;
+    wrapper.inState("ready", (s) => {
+      callCount++;
+      expect(s).toBe("ready");
+    });
+    emitter.setState("ready"); // should trigger callback
+    emitter.setState("idle"); // should NOT trigger callback again
+    emitter.setState("ready"); // should NOT trigger callback again
+    expect(callCount).toBe(1);
+  });
+
+  it("works with custom matcher for object state", () => {
+    type StateObj = { status: string };
+    class ObjectStateEmitter extends events.EventEmitter {
+      private _state: StateObj;
+      constructor(initialState: StateObj) {
+        super();
+        this._state = initialState;
+      }
+      getState() {
+        return this._state;
+      }
+      setState(newState: StateObj) {
+        this._state = newState;
+        this.emit("stateChange");
+      }
+    }
+    const initial = { status: "idle" };
+    const desired = { status: "ready" };
+    const emitter = new ObjectStateEmitter(initial);
+    const matcher = (a: StateObj, b: StateObj) => a.status === b.status;
+    const wrapper = new EmitterWrapper(emitter, { matcher });
+    let called = false;
+    wrapper.inState(desired, (s) => {
+      called = true;
+      expect(s).toEqual({ status: "ready" });
+    });
+    expect(called).toBe(false);
+    emitter.setState({ status: "ready" });
+    expect(called).toBe(true);
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,132 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import events from "events";
+
+export interface IEventEmitter<S = any> {
+  emit(event: string, ...args: any[]): void;
+  on(event: string, listener: (...args: any[]) => void): void;
+  off(event: string, listener: (...args: any[]) => void): void;
+  getState(): S;
+}
+
+export interface IInternalEventEmitter extends IEventEmitter {
+  afterAny(listener: (...args: any[]) => void): void;
+  ofAfterAny(listener: (...args: any[]) => void): void;
+}
+
+const ANY_EVENT = "InternalEventEmitter:ANY" as const;
+
+export class InternalEventEmitter<S = any>
+  extends events.EventEmitter
+  implements IInternalEventEmitter
+{
+  constructor(private emitter: IEventEmitter) {
+    super();
+    const previousEmiter = emitter.emit.bind(emitter);
+    emitter.emit = (event, ...args: any[]) => {
+      previousEmiter(event, ...args);
+      this.emit(ANY_EVENT);
+    };
+  }
+
+  static fromEventEmitter(emitter: IEventEmitter): InternalEventEmitter {
+    return new InternalEventEmitter(emitter);
+  }
+
+  afterAny(listener: (...args: any[]) => void): void {
+    this.on(ANY_EVENT, listener);
+  }
+
+  ofAfterAny(listener: (...args: any[]) => void): void {
+    this.off(ANY_EVENT, listener);
+  }
+
+  getState(): S {
+    return this.emitter.getState() as S;
+  }
+}
+
+export class EmitterWrapper<E extends IEventEmitter> {
+  private emitter: IInternalEventEmitter;
+  private originalEmitter: E;
+  private defaultTimeout?: number;
+  private matcher: (
+    fixedState: ReturnType<E["getState"]>,
+    state: ReturnType<E["getState"]>,
+  ) => boolean;
+  constructor(
+    emitter: E,
+    options: {
+      defaultTimeout?: number;
+      matcher?: (
+        fixedState: ReturnType<E["getState"]>,
+        state: ReturnType<E["getState"]>,
+      ) => boolean;
+    } = {},
+  ) {
+    this.originalEmitter = emitter;
+    this.defaultTimeout = options.defaultTimeout;
+    this.matcher =
+      options.matcher ?? ((fixedState, state) => state === fixedState);
+    this.emitter = InternalEventEmitter.fromEventEmitter(emitter);
+  }
+
+  static wrap<T extends IEventEmitter>(emitter: T): EmitterWrapper<T> {
+    return new EmitterWrapper(emitter);
+  }
+
+  getState(): ReturnType<E["getState"]> {
+    return this.emitter.getState();
+  }
+
+  inState<R extends ReturnType<E["getState"]>>(
+    state: R,
+    callback: (state: R, emitter: E) => any,
+  ): EmitterWrapper<E> {
+    const currentState = this.getState();
+    if (this.matcher(state, currentState)) {
+      callback(currentState, this.originalEmitter);
+      return EmitterWrapper.wrap(this.originalEmitter);
+    }
+
+    const listener = () => {
+      const localCurrentState = this.getState();
+      if (this.matcher(state, localCurrentState)) {
+        callback(localCurrentState, this.originalEmitter);
+        this.emitter.ofAfterAny(listener);
+      }
+    };
+    this.emitter.afterAny(listener);
+
+    return EmitterWrapper.wrap(this.originalEmitter);
+  }
+
+  promised<R extends ReturnType<E["getState"]>>(
+    state: R,
+    options: {
+      timeout?: number;
+    } = {},
+  ): Promise<E> {
+    const abort = new AbortController();
+    const timeout = options.timeout ?? this.defaultTimeout;
+    const timeoutPromise = timeout
+      ? new Promise<E>((_, reject) => {
+          setTimeout(() => {
+            abort.abort();
+            reject(new Error("Timeout"));
+          }, timeout);
+        })
+      : null;
+
+    return Promise.race<E>([
+      new Promise((resolve) => {
+        this.inState(state, (_, emitter) => {
+          abort.abort();
+          resolve(emitter);
+        });
+      }),
+      ...(timeoutPromise ? [timeoutPromise] : []),
+    ]);
+  }
+}
